882c9e0197932ffd7b0bfd470d06eb1e5a2fa5fa
Luis <dojas64@gmail.com>
2021-04-29 14:56:12 -0500

    Bugs

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #Stopping existing node servers
-echo "Stopping any existing node servers...Holi...."
+echo "Stopping any existing node servers...Holi...dd."
 cd /var/www/html/
 pm2 stop app.js
